75b40e6f9ccf84e4d2ff82315bc5d7d701a2502a
Supriya Bhurle <supriyabhurle28@gmail.com>
2022-12-24 23:30:02 +0530

    stack added

---
+++ src/com/bridgelabz/MyStackMain.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+public class MyStackMain {
+    public static void main(String[] args) {
+        MyStack myStack = new MyStack();
+        myStack.push(70);
+        myStack.push(30);
+        myStack.push(56);
+        myStack.display();
+
+    }
+}
